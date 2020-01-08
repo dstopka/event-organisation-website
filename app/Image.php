4818ffb1077c88dd22d82e7412ab475cb5d734f3
Karol Zborowski <krlzborowski@gmail.com>
2020-01-08 22:06:05 +0100

    Implement Image model

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    public function event(){
+        return $this->belongsTo('App\Event');
+    }
+
+    protected $fillable = [
+        'image',
+    ];
+
+
 }
