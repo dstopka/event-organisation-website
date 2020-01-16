ca05eaa92921110185091c1173f161ec45bd8593
Karol Zborowski <krlzborowski@gmail.com>
2020-01-16 01:50:58 +0100

    Set up codeception

---
+++ tests/acceptance/DeleteEventCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class DeleteEventCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
